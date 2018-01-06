8d129a911f473c3043575e52c3d0e8b07d5c9855
Gren Cissé Al-Karid <grencisse@hotmail.com>
2018-01-06 02:33:07 +0000

    Info UI page added

--- app/src/main/java/com/esatic/grenciss/androidiptk/MainActivity.java
+++ app/src/main/java/com/esatic/grenciss/androidiptk/MainActivity.java
@@ -1,14 +1,55 @@
 package com.esatic.grenciss.androidiptk;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    IPv4 iPv4;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getSupportActionBar().setElevation(0);
+
+        this.iPv4 = new IPv4();
+
+        Button btnValider = (Button)findViewById(R.id.btnValider);
+        final TextView txtIP = (TextView)findViewById(R.id.txtIP);
+        TextView txtMSQ = (TextView)findViewById(R.id.txtMSQ);
+
+        btnValider.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!iPv4.validateIPAddress(String.valueOf(txtIP.getText())))
+                {
+                    AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(MainActivity.this);
+                    alertDialogBuilder.setMessage("L'adresse IP est invalide");
+                        alertDialogBuilder.setPositiveButton("Okay",
+                            new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface arg0, int arg1) {
+
+                                }
+                            });
+
+                    AlertDialog alertDialog = alertDialogBuilder.create();
+                    alertDialog.show();
+                    return;
+                }
+
+                Intent I = new Intent(MainActivity.this, Main2Activity.class);
+                startActivity(I);
+            }
+        });
     }
 }
