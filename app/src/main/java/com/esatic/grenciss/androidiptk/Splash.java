0585e66773ccaab5cd1e68499f3428fb70a3ff00
Gren Cissé Al-Karid <grencisse@hotmail.com>
2018-01-06 04:14:37 +0000

    Lottie added

---
+++ app/src/main/java/com/esatic/grenciss/androidiptk/Splash.java
@@ -0,0 +1,23 @@
+package com.esatic.grenciss.androidiptk;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent I = new Intent(Splash.this, MainActivity.class);
+                startActivity(I);
+            }
+        }, 3500);
+    }
+}
